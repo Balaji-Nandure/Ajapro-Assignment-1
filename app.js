4dc4268e6111eac519bed9c15e22b084bfb4f29c
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-03-23 09:26:52 +0530

    till now working fine

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 /** @format */
-
-console.log("Script Working");
+/* ---------------------------------- Modal --------------------------------- */
+const products = [
+    {
+        productId: 0,
+        image: "https://rukminim1.flixcart.com/image/612/612/l27wtjk0/shirt/j/u/b/m-wnsh0015-wrogn-original-imagdmfjaeauzuzg.jpeg?q=70",
+        title: "Shirt 1",
+        price: 1281,
+    },
+    {
+        productId: 1,
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/8/8/g/m-wnsh2492-wrogn-original-imaghucpgzwdqzt7.jpeg?q=70",
+        title: "Shirt 2",
+        price: 1314,
+    },
+    {
+        productId: 2,
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/c/n/i/xxl-st10-vebnor-original-imagmtfju8vttgzf.jpeg?q=70",
+        title: "Shirt 3",
+        price: 914,
+    },
+    {
+        productId: 3,
+        image: "https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/f/n/r/m-wmsh0043-wrogn-original-imagg9xm5jnxygwf.jpeg?q=70",
+        title: "Shirt 4",
+        price: 814,
+    },
+];
