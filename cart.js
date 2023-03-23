efd5c0fafb1d3f25b64b85d04120ff8401c5a898
Balaji-Nandure <110956558+Balaji-Nandure@users.noreply.github.com>
2023-03-24 00:32:22 +0530

    remove from cart functionality also working great

--- cart.js
+++ cart.js
@@ -1,44 +1,37 @@
 /** @format */
 
 const cartTbody = document.getElementById("cart-tbody");
-cartTbody.innerHTML = "";
-// console.log(cart);
 
-cart.forEach((product) => {
-    // console.log(product);
-    const trToRender = `<tr>
-    <th class="text-center align-middle" scope="row">
-        <a id="remove-button" href="#"><i class="bi bi-x-circle"></i></a>
-    </th>
-    <td class="align-middle">
+// Render the cart
+function renderCart() {
+    cartTbody.innerHTML = "";
+    cart.forEach((product, index) => {
+        const trToRender = `<tr>
+      <th class="text-center align-middle" scope="row">
+        <a id="remove-button-${index}" href="#"><i class="bi bi-x-circle"></i></a>
+      </th>
+      <td class="align-middle">
         <img
-            class="cart-img"
-            src="${product.image}"
-            alt="Shirt Photo" />
-    </td>
-    <td class="align-middle">${product.title}</td>
-    <td class="align-middle">₹${product.price}</td>
-    <td class="align-middle">${product.quantity}</td>
-    <td class="align-middle">₹${product.quantity * product.price}</td>
+          class="cart-img"
+          src="${product.image}"
+          alt="Shirt Photo" />
+      </td>
+      <td class="align-middle">${product.title}</td>
+      <td class="align-middle">₹${product.price}</td>
+      <td class="align-middle">${product.quantity}</td>
+      <td class="align-middle">₹${product.quantity * product.price}</td>
     </tr>`;
 
-    cartTbody.innerHTML += trToRender;
-});
+        cartTbody.innerHTML += trToRender;
 
-/* --------------------- Remove from cart functionality --------------------- */
-const removeButton = document.getElementById("remove-button");
-console.log(removeButton);
+        // Add event listener to remove button
+        const removeButton = document.getElementById(`remove-button-${index}`);
+        removeButton.addEventListener("click", () => {
+            cart.splice(index, 1);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            renderCart();
+        });
+    });
+}
 
-removeButton.addEventListener("click", () => {
-    const cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-    const removeIndex = cart.findIndex(
-        (item) => item.productId === "product-1"
-    );
-
-    if (removeIndex !== -1) {
-        cart.splice(removeIndex, 1);
-    }
-
-    localStorage.setItem("cart", JSON.stringify(cart));
-});
+renderCart();
